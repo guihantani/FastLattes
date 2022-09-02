6b5c85bd82c4fc1288906b3e64a964cc7d1851a2
Guilherme H <ghantani@gmail.com>
2022-09-01 21:05:03 -0300

    add: function "initialize_researchers"
    fixed all affected functions

--- functions.py
+++ functions.py
@@ -1,50 +1,66 @@
 import os
 import xml.etree.ElementTree as ET
 from zipfile import ZipFile
-from pip._internal.utils.unpacking import unzip_file
 from researcher import Researcher
+import matplotlib.pyplot as plt
+import numpy as np
 
-def extract_authors_from_header(xml):
-    r = Researcher(xml)
-
-    return r.name
-
-def extract_works_years_from_header(xml):
-    r = Researcher(xml)
-
-    return r.works_years
-
-
-def extract_all_citations(path):
+def initialize_researchers(path):
     name_of_all_zip_files = os.listdir(path + '/.')
-    all_author_names = []
+    researchers = []
 
     for file in name_of_all_zip_files:
         os.chdir(path)
         with ZipFile(file, 'r') as z:
-            z.extractall() # unzip file
+            z.extractall()  # unzip file
 
         file_path = path + '/curriculo.xml'
-        xml = ET.parse(file_path).getroot() # load file
-        author = extract_authors_from_header(xml) # extract names
-        all_author_names.append(author)
-        os.remove(file_path) # remove unzip file
+        xml = ET.parse(file_path).getroot()  # load file
+        r = Researcher(xml)
+        researchers.append(r)
+        os.remove(file_path)  # remove unzip file
+
+    return researchers
+
+def generate_works_per_year_graphic(researchers):
+    all_works_per_year = np.array(extract_all_works_years(researchers))
+    all_works_per_year.sort()
+
+    unique_all_works_per_year = []
+    all_works_per_year_count = []
+
+    for x in all_works_per_year:
+        if x not in unique_all_works_per_year:
+            unique_all_works_per_year.append(x)
+            all_works_per_year_count.append(0)
+
+    for x in all_works_per_year:
+        index = unique_all_works_per_year.index(x)
+        all_works_per_year_count[index] += 1
+
+    fig, ax = plt.subplots(figsize=(len(unique_all_works_per_year), max(all_works_per_year_count) - 3))
+    ax.bar(unique_all_works_per_year, all_works_per_year_count, edgecolor="black")
+    for i in range(len(unique_all_works_per_year)):
+        ax.text(i, all_works_per_year_count[i], all_works_per_year_count[i], ha="center")
+
+    plt.xlabel('Anos')
+    plt.ylabel('Número de Trabalhos')
+
+    return fig
+
+def extract_all_citations(researchers):
+    all_author_names = []
+
+    for researcher in researchers:
+        all_author_names.append(researcher.name)
 
     return all_author_names
 
-def extract_all_works_years(path):
-    name_of_all_zip_files = os.listdir(path + '/.')
+def extract_all_works_years(researchers):
     all_works_years_names = []
 
-    for file in name_of_all_zip_files:
-        os.chdir(path)
-        with ZipFile(file, 'r') as z:
-            z.extractall() # unzip file
-
-        file_path = path + '/curriculo.xml'
-        xml = ET.parse(file_path).getroot() # load file
-        years = extract_works_years_from_header(xml) # extract works years
-        all_works_years_names = all_works_years_names + years
-        os.remove(file_path) # remove unzip file
+    for researcher in researchers:
+        all_works_years_names = all_works_years_names + researcher.works_years
 
     return all_works_years_names
+
--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 import os
 import functions as functions
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
 cur_path = os.path.dirname(__file__)
 new_path = cur_path + '\lattes'
 
-print(functions.extract_all_citations(new_path))
+researchers = functions.initialize_researchers(new_path)
+researchers_names = functions.extract_all_citations(researchers)
+fig = functions.generate_works_per_year_graphic(researchers)
 
-# Make works per year graphic
-y = np.array(functions.extract_all_works_years(new_path))
-y.sort()
+# Generate WebApp
+researchers_names.insert(0, 'Dados Gerais')
 
-unique_y = []
-y_count = []
-
-for x in y:
-    if x not in unique_y:
-        unique_y.append(x)
-        y_count.append(0)
-
-for x in y:
-    index = unique_y.index(x)
-    y_count[index] += 1
-
-fig, ax = plt.subplots(figsize=(len(unique_y), max(y_count) - 3))
-ax.bar(unique_y, y_count, edgecolor = "black")
-for i in range(len(unique_y)):
-    ax.text(i, y_count[i], y_count[i], ha = "center")
-
-plt.xlabel('Anos')
-plt.ylabel('Número de Trabalhos')
-
-st.title('Dados gerais do Lattes')
 st.sidebar.title('Menu')
-st.pyplot(fig)
+selectedPage = st.sidebar.selectbox('Selecione a página', researchers_names)
+
+if selectedPage == 'Dados Gerais':
+    st.title('Dados Gerais')
+    st.pyplot(fig)
+else:
+    st.title(selectedPage)
+
